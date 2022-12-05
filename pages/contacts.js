fc584f44195041520d15a8d7bf7ba6818ffe229e
Eduard <eduard.korytnyk@gmail.com>
2022-12-05 16:38:04 +0200

    add contanc page

--- pages/contacts.js
+++ pages/contacts.js
@@ -1,14 +1,17 @@
 import Link from "next/link";
 import ChristmasLights from "../components/christmasLights";
 import Nav from "../components/nav";
-import styles from '../styles/Home.module.css'
+import styles from '../styles/ContactPage.module.css'
 
 const Contacts = () => {
     return (
         <div>
         <ChristmasLights />
         <Nav />
-            Contacts
+            <div className={styles.contactWrap}>
+                <span>Виникли запитання ? Звертайся в телеграм </span>
+                <a rel="shortcut icon noopener noreferrer" target="_blank" href={'https://t.me/Eduard_Kop'}> 📱 {'t.me/Eduard_Kop'}</a>
+            </div>
         </div>
     )
 }
